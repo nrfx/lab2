7bfc0270afbfe997556737a596d03234ffff2af9
nrfx <57629383+nrfx@users.noreply.github.com>
2024-10-12 21:02:02 +0800

    1st and 2nd codes

---
+++ lab2.1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	float n, x;
+	int k;
+	//	Точное значение f_a (x), используя библиотечные математические функции; 
+	cout << "введите х (|x|<1): "; cin >> x;
+	cout << "введите n: "; cin >> n;
+
+	float u = x;
+	float y = 0;
+	k = 0;
+	//цикл for
+	for (k = 1; k <= n; k++) {
+		y += u;
+		u = (pow(x,2*k))/ (tgamma(2*k + 1));
+	}
+	cout << "y= " << y << endl;
+
+	u = x;
+	y = 0;
+	k = 0;
+
+	//цикл while 
+	cout << "через цикл while" << endl;
+
+	while (k < n) {
+		k++;
+		y += u;
+		u = (pow(x, 2 * k)) / tgamma(2 * k + 1);
+	}
+	cout << "y= " << y << endl;
+
+	//цикл do while
+
+	u = x;
+	y = 0;
+	k = 0;
+
+	cout << "через цикл do while" << endl;
+	do {
+		k++;
+		y += u;
+		u = (pow(x, 2 * k)) / tgamma(2 * k + 1);
+	} while (k < n);
+	cout << "y= " << y << endl;
+}
+
+	// Вычислить сумму с заданной погрешностью eps
+	//float  eps;
+	//cin >> "Введите eps= " >> eps >> endl;
+//}
---
+++ lab2.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n;
+	cout << "введите N: " << endl;
+	cin >> n;
+	if (n > 0) {
+		for (float x = 1; x < n; x++) {
+			for (float y = 1; y < n; y++) {
+				for (float z = 1; z < n; z++) {
+					if (pow(x, 2) + pow(y, 2) + pow(z, 2) == n)
+						cout << "x = " << x << " y = " << y << " z = " << z << endl;
+				}
+			}
+		}
+	}
+	else
+		cout << "число n меньше нуля" << endl;
+}
